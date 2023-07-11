6c40d1d4db12ffc6d80eafebdce455919b91f519
cihangir <cihangir@sunnyvalley.io>
2023-07-11 14:58:09 +0300

    tlsproxy/test: add a unit test for bitsSize type

---
+++ tlsserver/unitsbw_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func TestBitsSize(t *testing.T) {
+	tests := []struct {
+		bps  bitsSize
+		want string
+	}{
+		{4 * Mbits, "4Mbits"},
+		{2*Mbits + 500*Kbits, "2.5Mbits"},
+		{7 * Gbits, "7Gbits"},
+		{1000 * Mbits, "1Gbits"},
+	}
+	for _, tt := range tests {
+		tt := tt
+		got := fmt.Sprintf("%s", tt.bps)
+		if got != tt.want {
+			t.Errorf("bitsSizeStr(%d) = %q, want %q", tt.bps, got, tt.want)
+		}
+	}
+}
