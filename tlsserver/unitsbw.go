3118262843cdac0f7ec0da9e6870703e5826af9b
cihangir <cihangir@sunnyvalley.io>
2023-07-11 14:57:10 +0300

    tlsproxy: add a simple bandwidth/bps formatter

---
+++ tlsserver/unitsbw.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+type bitsSize int64
+
+const (
+	bits  bitsSize = 1
+	Kbits          = 1000 * bits
+	Mbits          = 1000 * Kbits
+	Gbits          = 1000 * Mbits
+	Tbits          = 1000 * Gbits
+
+	ToBits = 8
+)
+
+func (b bitsSize) String() string {
+	return bitsSizeStr(float64(b))
+}
+
+var (
+	bitspsAbbrs = []string{"bits", "Kbits", "Mbits", "Gbits", "Tbits"}
+)
+
+// bitsSize returns a human-readable size in bits, kbits,
+// megabits, gigabits (eg. "44Kbits", "17Mbits").
+func bitsSizeStr(size float64) string {
+	return customSize("%.4g%s", size, 1000.0, bitspsAbbrs)
+}
+
+// customSize returns a human-readable approximation of a size
+// using custom format.
+func customSize(format string, size float64, base float64, _map []string) string {
+	size, unit := getSizeAndUnit(size, base, _map)
+	return fmt.Sprintf(format, size, unit)
+}
+
+func getSizeAndUnit(size float64, base float64, _map []string) (float64, string) {
+	i := 0
+	unitsLimit := len(_map) - 1
+	for size >= base && i < unitsLimit {
+		size = size / base
+		i++
+	}
+	return size, _map[i]
+}
